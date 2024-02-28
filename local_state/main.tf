6925ef78bef449cea7366e4e99ac0abf4b07cbcf
adanilkumar <adanilkumar@outlook.com>
2024-02-28 15:18:50 +0000

    changed main.tf

--- local_state/main.tf
+++ local_state/main.tf
@@ -1,23 +1,13 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
-
-  required_version = ">= 1.2.0"
-}
-
 provider "aws" {
-  region  = "eu-west-2"
+  region = "eu-west-2"  # Change this to your desired region
 }
 
-resource "aws_instance" "terraform-server" {
-  ami           = "ami-0e5f882be1900e43b"
+resource "aws_instance" "example" {
+  ami           = "ami-0e5f882be1900e43b"  # Use a valid AMI ID
   instance_type = "t2.micro"
+  
 
   tags = {
-    Name = "Terraform_Demo"
+    Name = "example-instance"
   }
 }
